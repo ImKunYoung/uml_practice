0efefa5af88f0cdd5a237534d5c38c4e8e8c1d2d
ImKunYoung <ur2kunyoung2@outlook.com>
2023-07-03 09:26:28 +0900

    generalization

---
+++ src/chapter1/Dog.java
@@ -0,0 +1,16 @@
+package chapter1;
+
+/**
+ * @author : ImKunYoung
+ * @packageName : chapter1
+ * @fileName : Dog
+ * @since : 2023-07-03
+ * description    :
+ * <p>
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-07-03        ImKunYoung       최초 생성
+ */
+public class Dog extends Animal {
+}
